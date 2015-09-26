ad96f8f567b62463e5bff98670dd4837bfe5f6c4
Dr Nic Williams <drnicwilliams@gmail.com>
2015-09-26 06:46:05 -0700

    fix terraform/aws error: aws_security_group.pcfVMs: from_port (0) and to_port (65535) must both be 0 to use the the 'ALL' -1 protocol

--- aws-s3.tf
+++ aws-s3.tf
@@ -194,7 +194,7 @@ resource "aws_security_group" "pcfVMs" {
 
 	ingress {
 		from_port = 0
-		to_port = 65535
+		to_port = 0
 		protocol = "-1"
 		cidr_blocks = ["${aws_vpc.pcf-vpc.cidr_block}"]
 	}
